bd6418934b00d1b503a3c82e8ee3303c9f170b35
Murali-umamaheshwar <kurellaumamaheshwar7@gmail.com>
2022-04-08 12:40:09 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,16 @@ import { RegisterUserComponent } from './register-user/register-user.component';
 
 
 const routes: Routes = [
-    {path:"**",component:RegisterUserComponent},
-    {path:"home",component:HomeComponent}
+    
+    {path:"home",component:HomeComponent},
+    {path: '', redirectTo: '/register-user', pathMatch: 'full'},
+    {path:"register-user", component:RegisterUserComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  
+ }
--- src/app/register-user/register-user.component.ts
+++ src/app/register-user/register-user.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
-
-
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CustomValidators } from '../custom.validator';
 
 @Component({
   selector: 'app-register-user',
@@ -16,14 +14,26 @@ export class RegisterUserComponent implements OnInit {
     
   }
 
-  OnRegister(){
-    alert("Register Successfully")
-  }
-
+  
   OnLogin(){
     alert("Login Successfully")
   }
- 
+
+  registerForm = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    username: new FormControl(null, [Validators.required]),
+    firstname: new FormControl(null, [Validators.required]),
+    lastname: new FormControl(null, [Validators.required]),
+    password: new FormControl(null, [Validators.required,Validators.pattern("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}")]),
+    passwordConfirm: new FormControl(null, [Validators.required])
+  },
+    // add custom Validators to the form, to make sure that password and passwordConfirm are equal
+    { validators: CustomValidators.passwordsMatching }
+  )
+
+  register(){
+    alert("registration completed")
+  }
 
   
 }
